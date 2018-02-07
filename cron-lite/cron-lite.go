872e8894df641de57837ac175c0954e4c35b8ac2
Sébastien Portebois <sebastien.portebois@aerial.ai>
2018-02-07 15:51:38 -0500

    Fix formatting

--- cron-lite/cron-lite.go
+++ cron-lite/cron-lite.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"os"
-	"os/exec"
-	"log"
 	"flag"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/robfig/cron"
 	"gopkg.in/yaml.v2"
-
 )
 
 type ScheduledJob struct {
-	Command string	`yaml:"command"`
-	Args	string  `yaml:"args"`
-	Rule	string  `yaml:"rule"`
+	Command string `yaml:"command"`
+	Args    string `yaml:"args"`
+	Rule    string `yaml:"rule"`
 }
 
 func (sj ScheduledJob) Run() {
 	log.Print(sj)
-	cmd := exec.Command(sj.Command, sj.Args);
+	cmd := exec.Command(sj.Command, sj.Args)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
@@ -37,10 +36,9 @@ func main() {
 	fnamePtr := flag.String("f", "", "YAML file name with rules")
 	flag.Parse()
 
-	if *fnamePtr=="" {
+	if *fnamePtr == "" {
 		log.Fatal("Please specify filename with -f")
 	}
-
 
 	log.Printf("cron-lite v%s by Quantverse", version)
 	log.Printf("loading config from %s", *fnamePtr)
@@ -62,14 +60,14 @@ func main() {
 	c := cron.New()
 	for _, item := range m {
 		log.Printf("adding new job: [%s %s]: %s\n", item.Command, item.Args, item.Rule)
-		c.AddJob(item.Rule, item);
-		cnt++;
+		c.AddJob(item.Rule, item)
+		cnt++
 	}
 	log.Printf("%d jobs added\n", cnt)
 	if cnt > 0 {
 		c.Start()
 		log.Print("cron engine started")
-		select{}
+		select {}
 	}
 
 }
